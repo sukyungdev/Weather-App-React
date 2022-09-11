ab7792b6c2c0cd5137c3b7d8b562f21a247247fa
sukyungdev <sukyung.developer@gmail.com>
2022-09-11 15:38:39 +0900

    Setting eslint

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import "./App.css";
-import { useEffect, useCallback } from "react";
-import styled from "styled-components";
+import './App.css';
+import { useEffect, useCallback } from 'react';
+import styled from 'styled-components';
 
 function App() {
-    const getCurrentLocation = useCallback(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude;
-            getWeatherByCurrentLocation(lat, lon);
-        });
-    }, []);
+  const getCurrentLocation = useCallback(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  }, []);
 
-    const getWeatherByCurrentLocation = async (lat, lon) => {
-        console.log(lat, lon);
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=26838e13d923034d329d7992ddfe3746`;
-        let response = await fetch(url);
-        let data = await response.json();
-        console.log(data);
-    };
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    console.log(lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=26838e13d923034d329d7992ddfe3746`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  };
 
-    useEffect(() => {
-        getCurrentLocation();
-    }, [getCurrentLocation]);
+  useEffect(() => {
+    getCurrentLocation();
+  }, [getCurrentLocation]);
 
-    return <Container></Container>;
+  return <Container />;
 }
 const Container = styled.main`
-    width: 100%;
-    height: 100vh;
-    background-size: cover;
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
 `;
 
 export default App;
