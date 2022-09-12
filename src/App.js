53e28170fc4f68d78e9fb52787332961d2b16be9
sukyungdev <sukyung.developer@gmail.com>
2022-09-12 23:24:25 +0900

    [Feature] getWeatherForecast

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ function App() {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
       getWeatherByCurrentLocation(lat, lon);
+      getWeatherForecast(lat, lon);
     });
   }, []);
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    console.log(lat, lon);
+    // console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=26838e13d923034d329d7992ddfe3746&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
     setWeather(data);
     // console.log(weather);
+  };
+
+  const getWeatherForecast = async (lat, lon) => {
+    console.log(lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=26838e13d923034d329d7992ddfe3746&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('Forecast', data);
   };
 
   useEffect(() => {
